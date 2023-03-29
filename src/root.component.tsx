a9ff27b1c8eb9275d7285ec9a42a844e5ecbfeb3
AkshayPadawan <thodakshaytinku5@gmail.com>
2023-03-29 16:58:41 +0530

    reactSpa initial

---
+++ src/root.component.tsx
@@ -0,0 +1,20 @@
+export default function Root(props) {
+  return (
+    <section>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          padding: "20px",
+          color: "white",
+          backgroundColor: "blue",
+        }}
+      >
+        <div>
+          <h2>React project </h2>
+        </div>
+      </div>
+    </section>
+  );
+}
